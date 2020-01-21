bfc5361e5e9ab8fde9eef269567d781fb12c58a9
bosko <bosko.petreski@gmail.com>
2020-01-21 13:52:52 +0100

    Initial Commit

---
+++ VESC_IOS_SWIFT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VESC_IOS_SWIFT
+//
+//  Created by Bosko Petreski on 1/21/20.
+//  Copyright © 2020 Bosko Petreski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
